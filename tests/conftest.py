fec06a8c30ab976604af7925bd113a742869e4ac
Erez-Shlingbaum <erez.shlingbaum@gmail.com>
2022-01-03 20:03:05 +0200

    Fix CI path for pytest

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,13 +1,6 @@
 """
-    Dummy conftest.py for empire.
-
-    If you don't know what this is for, just leave it empty.
-    Read more about conftest.py under:
     - https://docs.pytest.org/en/stable/fixture.html
     - https://docs.pytest.org/en/stable/writing_plugins.html
 """
-import pytest
 
-@pytest.fixture(autouse=True)
-def change_test_dir(request, monkeypatch):
-    monkeypatch.chdir(request.fspath.dirname)
+# import pytest
