6046884ff5104106f721f2bf7a7e6386d06cbacb
Erez-Shlingbaum <erez.shlingbaum@gmail.com>
2022-01-06 22:27:57 +0200

    Implement draft for zoom and scroll

--- src/game.py
+++ src/game.py
@@ -14,10 +14,42 @@ class Game(pyglet.window.Window):
                          fullscreen=config.fullscreen, *args, **kwargs)
         self.is_game_done = False
         self.world = world.World()
+        self.fps_display = pyglet.window.FPSDisplay(self)
+        self.keys = pyglet.window.key.KeyStateHandler()
+        self.push_handlers(self.keys)
+        self.zoom = 1
+
+    def on_key_press(self, symbol, modifiers):
+        if symbol == pyglet.window.key.ESCAPE:
+            self.close()
+
+    def on_mouse_scroll(self, x, y, scroll_x, scroll_y):
+        zoom = 1.00
+        if scroll_y > 0:
+            zoom = 0.97
+        elif scroll_y < 0:
+            zoom = 1.03
+
+        pyglet.gl.glOrtho(-zoom, zoom, -zoom, zoom, -1, 1)
 
     def update(self, delta_time_ms: float):
+        if self.keys[pyglet.window.key.A]:
+            pyglet.gl.glTranslatef(10, 0, 0)
+        if self.keys[pyglet.window.key.W]:
+            pyglet.gl.glTranslatef(0, -10, 0)
+        if self.keys[pyglet.window.key.D]:
+            pyglet.gl.glTranslatef(-10, 0, 0)
+        if self.keys[pyglet.window.key.S]:
+            pyglet.gl.glTranslatef(0, 10, 0)
+
         self.world.update(delta_time_ms)
 
     def on_draw(self):
+        pyglet.gl.glPushMatrix()
+        pyglet.gl.glScalef(self.zoom, self.zoom, 1)
+
         self.clear()
         self.world.draw()
+        self.fps_display.draw()
+
+        pyglet.gl.glPopMatrix()
